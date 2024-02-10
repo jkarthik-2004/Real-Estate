97720cecf0877e85610cd9bfba3e5e8da6de00ed
karthik <jkarthik1947@gmail.com>
2024-02-10 22:59:39 +0530

    created listing item component and show listings

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -99,7 +99,7 @@ export default function Listing() {
               {listing.type === 'rent' && ' / month'}
             </p> */}
             <p className='text-2xl font-semibold'>
-  {listing.name} - ${' '}
+  {listing.name} - ₹{' '}
   {listing.offer && listing.discountPrice !== undefined
     ? listing.discountPrice.toString()  // Convert to string
     : listing.regularPrice !== undefined
@@ -117,7 +117,7 @@ export default function Listing() {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountedPrice}
+                  ₹{listing.discountedPrice} Off
                 </p>
               )}
 
