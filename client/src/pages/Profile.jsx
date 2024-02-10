4dcc9da14485a251a7de135121201279dd964898
karthik <jkarthik1947@gmail.com>
2024-02-10 15:06:27 +0530

    completed delete user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -131,7 +131,23 @@ export default function Profile() {
     }
   }
 
-
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`,
+      {
+        method: 'DELETE',
+      }
+      )
+      const data = await res.json()
+      if(data.success === false) {
+        console.log(data.message)
+        return
+      }
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId))
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
 
 
   return (
@@ -192,7 +208,7 @@ export default function Profile() {
             <p>{listing.name}</p>
           </Link>
           <div className='flex flex-col items-center'>
-            <button className='text-red-700 uppercase'>Delete</button>
+            <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
             <button className='text-green-700 uppercase'>Edit</button>
           </div>
         </div> )}
